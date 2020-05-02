5909565d0aacedc7012880348984b294c68e2b29
sstrazdina <strazdinakathrine@gmail.com>
2020-05-02 12:04:16 +0300

    lab5 - lava servlets

---
+++ JavaServlets/src/main/java/ServletStatus.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletStatus extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<a style=\"color:red;\">Application is running</a>");
+    }
+}
